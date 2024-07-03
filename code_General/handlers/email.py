6100ce19c2c2e9e7a7af68ec764c8248e926ff9d
Akshay NS <shewatkar@infai.org>
2024-07-03 12:23:46 +0200

    error handling & paths in email.py

--- code_General/handlers/email.py
+++ code_General/handlers/email.py
@@ -12,12 +12,34 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_http_methods
 from ..connections.mailer import MailingClass
 from django.conf import settings
+from ..utilities.basics import ExceptionSerializer
+from rest_framework import status, serializers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from drf_spectacular.utils import extend_schema
+
 from logging import getLogger
 
 logger = getLogger("django_debug")
 
-#######################################################
-@require_http_methods(["POST"])
+#########################################################################
+# sendContactForm
+#"contactForm": ("public/contact/",email.sendContactForm)
+#########################################################################
+#TODO Add serializer for sendContactForm
+#########################################################################
+# Handler  
+@extend_schema(
+    summary="Send an email from the contact form from the front end",
+    request=None,
+    tags=['email'],
+    responses={
+        200: None,
+        400: ExceptionSerializer,
+        500: ExceptionSerializer
+    },
+)
+@api_view(["POST"])
 def sendContactForm(request):
     """
     Send an email from the contact form from the front end
@@ -28,19 +50,24 @@ def sendContactForm(request):
     :rtype: JsonResponse
     """
 
-    # TODO check if logging is necessary
-    logger.info(f'received contact form input: "{str(request.body)}')
-    data = json.loads(request.body.decode("utf-8"))
-    # check if all fields are present
-    if not all(key in data for key in ["name", "email", "subject", "message"]):
-        return JsonResponse({"status": "error", "result": "missing fields"})
+    try:
+        # TODO check if logging is necessary
+        logger.info(f'received contact form input: "{str(request.body)}')
+        data = json.loads(request.body.decode("utf-8"))
+        # check if all fields are present
+        if not all(key in data for key in ["name", "email", "subject", "message"]):
+            return Response({"status": "error", "result": "missing fields"}, status=status.HTTP_400_BAD_REQUEST)
 
-    mailer = MailingClass()
-    msg = ("Backendsettings: " + settings.BACKEND_SETTINGS +
-           "\nName: " +
-           data["name"] +
-           "\n" +
-           "Email: " +
-           data["email"] + "\n" + "Message: " + data["message"])
-    result = mailer.sendMail(settings.EMAIL_ADDR_SUPPORT, data["subject"], msg)
-    return JsonResponse({"status": "ok", "result": result})
+        mailer = MailingClass()
+        msg = ("Backendsettings: " + settings.BACKEND_SETTINGS +
+               "\nName: " +
+               data["name"] +
+               "\n" +
+               "Email: " +
+               data["email"] + "\n" + "Message: " + data["message"])
+        result = mailer.sendMail(settings.EMAIL_ADDR_SUPPORT, data["subject"], msg)
+        return Response({"status": "ok", "result": result}, status=status.HTTP_200_OK)
+
+    except Exception as e:
+        logger.error(f'error sending contact form: {e}')
+        return Response({"status": "error", "result": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
